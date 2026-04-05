f2f50dfc757c53c70ab1ca2d1cf45557648d7b3a
arthurnc1004 <arthurnunesc1004@gmail.com>
2026-04-05 01:20:37 -0300

    adição do main.py e páginas HTML

---
+++ main.py
@@ -0,0 +1,69 @@
+from flask import Flask, request, render_template
+import sqlite3
+app = Flask(__name__)
+
+def criartabela():
+    conexao = sqlite3.connect('pedidos.db')
+    cursor = conexao.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS pedidos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        cliente TEXT,
+        tamanho TEXT,
+        massa TEXT,
+        recheio TEXT,
+        valor REAL
+    )
+    """)
+    conexao.commit()
+    conexao.close()
+
+criartabela()
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+@app.route('/admin')
+def admpage():
+    conexao = sqlite3.connect('pedidos.db')
+    cursor = conexao.cursor()
+    cursor.execute('SELECT * FROM pedidos')
+    pedidos = cursor.fetchall()
+    conexao.close()
+    return render_template('admin.html', pedidos=pedidos)
+
+@app.route('/pedido', methods=['POST'])
+def pedido():
+    nome = request.form['nome']
+    tamanho = request.form['tamanho']
+    massa = request.form['massa']
+    recheio = request.form['recheio']   
+    if tamanho == 'Pequeno':
+        valor = 50
+    elif tamanho == 'Medio':
+        valor = 70
+    else:
+        valor = 90
+    return render_template('revisar.html', nome=nome, tamanho=tamanho, massa=massa, recheio=recheio, valor=valor)
+
+@app.route('/confirmar', methods=['POST'])
+def confirmar():
+    nome = request.form['nome']
+    tamanho = request.form['tamanho']
+    massa = request.form['massa']
+    recheio = request.form['recheio']
+    valor = request.form['valor']
+    conexao = sqlite3.connect('pedidos.db')
+    cursor = conexao.cursor()
+    cursor.execute("""
+    INSERT INTO pedidos (cliente, tamanho, massa, recheio, valor)
+    VALUES (?, ?, ?, ?, ?)
+    """, (nome, tamanho, massa, recheio, valor))
+    conexao.commit()
+    conexao.close()
+    return render_template('confirmar.html', nome=nome, tamanho=tamanho, massa=massa, recheio=recheio, valor=valor)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
